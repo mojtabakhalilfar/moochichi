0d16e3d97dc24bb83fc58f7b158d918fb6e67147
mojtaba khalilfar <mojtabakhalilfar11@gmail.com>
2025-05-22 23:51:10 +0330

    i dont know

--- src/app/[category]/[product]/page.tsx
+++ src/app/[category]/[product]/page.tsx
@@ -7,17 +7,14 @@ import RelatedProduct from '@/components/product/RelatedProduct'
 import { TPParams, TProduct } from '@/types/Type'
 
 
-// import ProductDescription from '@/components/Products/Tesst'
 import React from 'react'
 
 const page = async ({ params }: TPParams) => {
 
   const { product } = await params
   const id: number = parseInt(product) % 11
-  // console.log(id)
   const result = await fetch(`http://localhost:8000/product?id=${id}`)
   const data = (await result.json())[0] as TProduct
-  // console.log(data.relatedproducts)
   return (
     <div className='flex items-center justify-center w-full'>
       <div className='flex flex-col items-center w-full h-auto'>
@@ -26,8 +23,6 @@ const page = async ({ params }: TPParams) => {
         <AboutProduct />
         <RelatedProduct mostpopular={data.relatedproducts}/>
         <Comments />
-        {/* <ProductDescription /> */}
-        {/* <Page /> */}
       </div>
     </div>
   )
